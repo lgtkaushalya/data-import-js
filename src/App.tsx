58c9a9e75c8aebf2a3239c21763fd9a802262e5c
Thilanka <thilanka@orangehrm.us.com>
2024-11-02 19:59:09 +0530

    Add file upload button to the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from "react";
 import logo from "./logo.svg";
+import Papa from "papaparse";
+
 import "./App.css";
 
 function App() {
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]; // Get the selected file
+
+    if (file) {
+      Papa.parse(file, {
+        header: true, // Parse the header row
+        complete: (results) => {
+          console.log("Parsed Data:", results.data); // Output the parsed data to the console
+        },
+        error: (error) => {
+          console.error("Error while parsing:", error); // Handle parsing errors
+        },
+      });
+    } else {
+      console.log("No file selected.");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-500">
-          Edit <code>src/App.tsx</code> and save to reload!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="input-file-container">
+      <label className="input-file">
+        Choose File
+        <input
+          type="file"
+          id="uploadCsv"
+          accept=".csv"
+          onChange={handleFileChange}
+        />
+      </label>
     </div>
   );
 }
